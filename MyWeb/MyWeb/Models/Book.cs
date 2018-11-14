798fb64fb15cabfc481bd2ee90d3698b30f06da0
W10415528 <n1237569608@gmail.com>
2018-11-14 19:50:41 +0800

    第四題
    Models右鍵->加入->新增項目->ASP.NET Core-> 類別: Book.cs
    
    public class BookContext : DbContext {
            public BookContext(DbContextOptions<BookContext> options)
            : base(options)
            { }
    
            public DbSet<Book> Books { get; set; }
        }
        public class Book
        {
            public String Name { get; set; }
            public String ISBN { get; set; }
            public int price  { get; set; }
         }
    }

---
+++ MyWeb/MyWeb/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWeb.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+        : base(options)
+        { }
+
+
+        public DbSet<Book> Books { get; set; }
+    }
+    public class Book
+    {
+        
+        public String Name { get; set; }
+        public String ISBN { get; set; }
+
+        public int price { get; set; }
+    }
+}
