eb30d18742b30dc53cbb80b4a9b05d383ee7c6f7
W10415528 <n1237569608@gmail.com>
2018-11-14 19:28:28 +0800

    第二題
    新增View底下
    mypage.cshtml.cshtml
    寫
    <h1>hello 410415528 @DateTime.Now</h1

--- MyWeb/MyWeb/Controllers/HomeController.cs
+++ MyWeb/MyWeb/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace MyWeb.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("~/Views/Home/mypage.cshtml");
         }
 
         public IActionResult About()
@@ -33,11 +33,13 @@ namespace MyWeb.Controllers
         {
             return View();
         }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
